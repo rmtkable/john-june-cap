05ea8d51524b1a8306134db3ce78a673fb11dc5a
Tyler Wellman <TangoKilo1985@gmail.com>
2022-08-29 16:25:30 -0400

    changes

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect('mongodb+srv://johns:password1234@cluster0.cr9e0xj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlparser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+
+const connection = mongoose.connection;
+connection.openUri('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+
+const locationRouter = require('./Routes/location');
+
+app.use('/location', locationRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: $(port)`);
+});
