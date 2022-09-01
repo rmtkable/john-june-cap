56c06d5d9b7b48c3968b6f0f489b7941cc99e53d
jschuess11 <108285498+jschuess11@users.noreply.github.com>
2022-09-01 11:18:34 -0400

    thursday

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb+srv://johns:password1234@cluster0.cr9e0xj.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect('mongodb+srv://johns:password1234@cluster0.cr9e0xj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlparser: true, useCreateIndex: true, useUnifiedTopology: true }
+mongoose.connect(uri, { useNewUrlparser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 
 const connection = mongoose.connection;
-connection.openUri('open', () => {
+connection.once('open', () => {
     console.log('MongoDB database connection established successfully');
 });
 
-const locationRouter = require('./Routes/locations');
-
-app.use('./locations', locationRouter);
+const attractionRouter = require('./Routes/attractions');
+const contactRouter = require('./Routes/contact')
+app.use('/attractions', attractionRouter);
+app.use('/contact', contactRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: $(port)`);
+    console.log(`Server is running on port: ${port}`);
 });
--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const path = require("path");
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const path = require("path");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+// const app = express();
+// const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+// const uri = "mongodb+srv://johns:password1234@cluster0.cr9e0xj.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(uri,{
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
-const connection = mongoose.connection;
+// mongoose.connect(uri,{
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true
+// });
+// const connection = mongoose.connection;
 
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
 
-const attractionsRouter = require("./backend/Routes/attractions");
-const contactRouter = require("./backend/routes/contact");
+// const attractionsRouter = require("./backend/Routes/attractions");
+// const contactRouter = require("./backend/Routes/contact");
 
-app.use("/attractions", attractionsRouter);
-app.use("/contact", contactRouter);
+// app.use("/attractions", attractionsRouter);
+// app.use("/contact", contactRouter);
 
-app.use(express.static(path.join(__dirname, "./build")));
+// // app.use(express.static(path.join(__dirname, "./build")));
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
