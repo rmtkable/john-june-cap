e76823aed6be669ec85977c9ca6570928556ee93
Tyler Wellman <TangoKilo1985@gmail.com>
2022-09-02 13:51:40 -0400

    icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import '../styles/home.css'
 import downtownPic from "../assets/downTownPic.jpg"
+import {FaChessQueen} from "react-icons/fa";
 
 const Home = () => {
   return (
     <div class="home">
+    <img src={downtownPic} alt="Downtown Cincinnati" />
+    
+    <p class="landing-text"><FaChessQueen /> Cincinnati: Queen of the Midwest! <FaChessQueen /></p>
+  
       <img src={downtownPic} alt="Downtown Cincinnati" />
       <p class="landing-text">Cincinnati: Queen of the Midwest!</p>
     </div>
