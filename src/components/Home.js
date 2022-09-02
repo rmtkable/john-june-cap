e84f5fefb8e3250bfb9009ddd04c050b068471a3
jschuess11 <108285498+jschuess11@users.noreply.github.com>
2022-09-02 11:59:08 -0400

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,13 @@
 import React from "react";
-import '../styles/home.css'
-import downtownPic from "../assets/downTownPic.jpg"
+import "../styles/home.css";
+import downtownPic from "../assets/downTownPic.jpg";
 
 const Home = () => {
   return (
-    // <div class="dtown">
     <div class="home">
-    <img src={downtownPic} alt="Downtown Cincinnati" />
-    <p class="landing-text">Cincinnati: Queen of the Midwest!</p>
+      <img src={downtownPic} alt="Downtown Cincinnati" />
+      <p class="landing-text">Cincinnati: Queen of the Midwest!</p>
     </div>
-    // </div>
   );
 };
 
