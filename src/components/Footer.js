8607e7f81bbe9204ed6d395d1857269aae3dddb9
Tyler Wellman <TangoKilo1985@gmail.com>
2022-09-06 16:03:21 -0400

    afternoon changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,31 @@
 import React from "react";
-import '../styles/footer.css'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import '../styles/footer.css';
 
 function Footer() {
   return (
     <div className="footer">
-          <h3>Capstone</h3>
-          <p>
-            Created by Kable Academy's 2022 Summer Bootcamp team of John
-            Schuessler and Tyler Wellman.
-          </p>
-        <div className="">
-          <h3>John Schuessler</h3>
-          <a href="https://github.com/jschuess11">Github</a>
-          <a href="https://www.linkedin.com/in/johnschuessler/">LinkedIn</a>
+          <div className="capstone">
+            <h3>Capstone</h3>
+            <p>
+              Website Created by Kable Academy's 2022 Summer Bootcamp team of John
+              Schuessler and Tyler Wellman. Follow us on Github and LinkedIn.
+            </p>
+          </div>
+        <div className="outer">
+          <div className="one">
+           <h3>John Schuessler</h3>
+           <a href="https://github.com/jschuess11" className="icons1"><FaGithub /></a>
+           <a href="https://www.linkedin.com/in/johnschuessler/" className="icons2"><FaLinkedinIn /></a>
+          </div>
+          <div className="two">
+            <h3>Tyler Wellman</h3>
+            <a href="https://github.com/TangoKilo85" className="icons1"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/tyler-wellman-776815243/" className="icons2"><FaLinkedinIn /></a>
+          </div>
         </div>
-        <div className="">
-          <h3>Tyler Wellman</h3>
-          <a href="https://github.com/TangoKilo85">Github</a>
-          <a href="https://www.linkedin.com/in/tyler-wellman-776815243/">
-            LinkedIn
-          </a>
-        </div>
-    </div>
+      </div>
   );
 }
 
