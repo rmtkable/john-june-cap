f7ec6a49e488b53d591fc16cdd9a87e5b25d3602
jschuess11 <108285498+jschuess11@users.noreply.github.com>
2022-09-08 13:58:24 -0400

    breweries

--- src/pages/brewsbars.js
+++ src/pages/brewsbars.js
@@ -11,14 +11,14 @@ export const Brewsbars = () => {
                         <h1>Breweries and Bars</h1>
                     </div>
                 </div>
-            </div>
+            
 
             <div className="brewHeader">
                 <h1>Check out some of the top Breweries and Bars in Cincinnati!</h1>
             </div>
 
             <div className="brewCards">
-                <div className="container card">
+                <div className="container-card">
                     <h5 className="card-title">Rhinegeist</h5>
                     <img src={rhinegeistPic} className="card-img-top" alt="Rhinegeist Brewery" />
                 </div>
@@ -26,6 +26,7 @@ export const Brewsbars = () => {
                     <p className="card-text">This large brewery and event space in historic Over-the-Rhine (housed in an old Moerlein bottling plant) produces hoppy and sessionable ales and Cidergeist, its line of crushable ciders. The seasonal deck is an excellent addition to the city’s rooftop bar scene. The first half of Rhinegeist’s name hails from its location in Cincinnati. The second half, “Geist,” is a German word that translates to “ghost” and a nod to the city’s German heritage.</p>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
