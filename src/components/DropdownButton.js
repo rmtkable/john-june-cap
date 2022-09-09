f272dd2e575e33440a2e9c4f5f20102d7acbdbf2
jschuess11 <108285498+jschuess11@users.noreply.github.com>
2022-09-09 09:27:51 -0400

    addressed error w/ dropdwon href

--- src/components/DropdownButton.js
+++ src/components/DropdownButton.js
@@ -6,13 +6,8 @@ function Button() {
   return (
     <DropdownButton id="dropdown-basic-button" title="Attractions" className='btn'>
       <Dropdown.Item href="/brewsBars">Breweries</Dropdown.Item>
-<<<<<<< HEAD
       <Dropdown.Item href="/food">Food</Dropdown.Item>
-      <Dropdown.Item href="#/entertainment">Entertainment</Dropdown.Item>
-=======
-      <Dropdown.Item href="#/food">Food</Dropdown.Item>
       <Dropdown.Item href="/entertainment">Entertainment</Dropdown.Item>
->>>>>>> 5d7c595c0077d1e4341cb3bde57a405d88d2f44b
       <Dropdown.Item href="#/artsMuseums">Arts & Museums</Dropdown.Item>
     </DropdownButton>
   );
