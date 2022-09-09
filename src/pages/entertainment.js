5d7c595c0077d1e4341cb3bde57a405d88d2f44b
Tyler Wellman <TangoKilo1985@gmail.com>
2022-09-09 09:11:58 -0400

    entertainment update

--- src/pages/entertainment.js
+++ src/pages/entertainment.js
@@ -1,5 +1,7 @@
 import React from "react";
 import kingsIsland from "../assets/kingsIsland.jpg"
+import coneyIslandPic from "../assets/coneyIslandPic.jpg"
+import cinZooPic from "../assets/cinZooPic.jpg"
 import '../styles/entertainment.css';
 
 export const Entertainment = () => {
@@ -12,7 +14,7 @@ export const Entertainment = () => {
                     </div>
                 </div>
             <div className="entertainmentHeader">
-                <h1>Looking for Fun? Check out these Entertainment Hotspots!</h1>
+                <h1>Looking for Fun? Check out these Entertaining Hotspots!</h1>
             </div>
 
             <div className="entertainmentCards">
@@ -25,6 +27,45 @@ export const Entertainment = () => {
                 </div>
                     <div class="card-body">
                     <a href="https://www.visitkingsisland.com/" target="_blank" rel="noreferrer" class="card-link">Website</a>
+                    </div>
+            </div>
+
+            <div className="entertainmentCards">
+                <div className="container-card">
+                    <h5 className="card-title">Coney Island Park</h5>
+                    <img src={coneyIslandPic} className="card-img-top" alt="Coney Island Park" />
+                </div>
+                <div className="card-body">
+                    <p className="card-text">Escape the summer heat with Cincinnati's Coney Island. You can swim in North America’s largest recirculating pool, race down more than ¼ of a mile of water slides, and create mammoth splashes in Cannonball Cove. Looking to really get drenched? You can play with Typhoon Tower’s 70 interactive spray-stations or get soaked by the world’s largest erupting hydro storm as it releases 1,300 gallons of cascading water from a 33-foot tower! On the weekends, if you’re looking for a change of pace, you can play a round of mini golf or enjoy tranquil Lake Como on a Storybook Paddleboat on Lake Como.</p>
+                </div>
+                    <div class="card-body">
+                    <a href="https://coneyislandpark.com/" target="_blank" rel="noreferrer" class="card-link">Website</a>
+                    </div>
+            </div>
+
+            <div className="entertainmentCards">
+                <div className="container-card">
+                    <h5 className="card-title">Linder Tennis Center</h5>
+                    <img src={coneyIslandPic} className="card-img-top" alt="Coney Island Park" />
+                </div>
+                <div className="card-body">
+                    <p className="card-text">The Lindner Family Tennis Center at Lunken Playfield (LTC), a Cincinnati Recreation Commission facility, provides many tennis activities to match your needs, interest and skill level. Recognized by Cincinnati Magazine in their “Best Of The City” December 2015 publication and by the USTA as the 2003 National Public Facility of the Year. LTC, a 16 court facility with 8 hard courts and 8 Har-Tru clay courts, is home to a city wide web based adult ladder league with four skill levels, 12 USTA Adult and Junior Tournaments including The Cincinnati Adult Championship (The Met), weekly social events, Junior Fun Camps and instruction led by USPTA Professionals and many other enriching tennis activities for the entire family. The LTC is the only public facility in the greater Cincinnati metro-plex that offers Har-Tru clay court play.</p>
+                </div>
+                    <div class="card-body">
+                    <a href="https://lindner.tenniscores.com/" target="_blank" rel="noreferrer" class="card-link">Website</a>
+                    </div>
+            </div>
+
+            <div className="entertainmentCards">
+                <div className="container-card">
+                    <h5 className="card-title">Cincinnati Zoo</h5>
+                    <img src={cinZooPic} className="card-img-top" alt="Cincinnati Zoo" />
+                </div>
+                <div className="card-body">
+                    <p className="card-text">The Cincinnati Zoo & Botanical Garden is the sixth oldest zoo in the United States, founded in 1873 and officially opening in 1875. It is located in the Avondale neighborhood of Cincinnati, Ohio. It originally began with 64.5 acres (26.1 ha) in the middle of the city, but has spread into the neighboring blocks and several reserves in Cincinnati's outer suburbs. It was appointed as a National Historic Landmark in 1987.</p>
+                </div>
+                    <div class="card-body">
+                    <a href="https://cincinnatizoo.org/" target="_blank" rel="noreferrer" class="card-link">Website</a>
                     </div>
             </div>
         </div>
