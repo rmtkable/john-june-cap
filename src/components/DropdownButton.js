7dc5974ed86fd141eb18fdb31ce428f58e221911
jschuess11 <108285498+jschuess11@users.noreply.github.com>
2022-09-09 15:54:58 -0400

    end of friday

--- src/components/DropdownButton.js
+++ src/components/DropdownButton.js
@@ -12,12 +12,8 @@ function Button() {
       <Dropdown.Item href="/brewsBars">Breweries</Dropdown.Item>
       <Dropdown.Item href="/food">Food</Dropdown.Item>
       <Dropdown.Item href="/entertainment">Entertainment</Dropdown.Item>
-<<<<<<< HEAD
-      <Dropdown.Item href="#/artsMuseums">Arts & Museums</Dropdown.Item>
-      <Dropdown.Item href="/attractions">VIEW ALL</Dropdown.Item>
-=======
       <Dropdown.Item href="/artsMuseums">Arts & Museums</Dropdown.Item>
->>>>>>> c10f5fa5b22c8762a586618176aad0b29ceba272
+      <Dropdown.Item href="/attractions">VIEW ALL</Dropdown.Item>
     </DropdownButton>
   );
 }
